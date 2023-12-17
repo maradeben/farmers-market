f2a1b447335ebc85ed90cf2815f2716c98ff7f5e
Eben <maradebenezer@gmail.com>
2023-12-17 06:55:04 +0100

    updatging to db storage

--- backend/database/db_storage.py
+++ backend/database/db_storage.py
@@ -3,13 +3,34 @@
 
 
 import mongoengine
-from models.base import BaseModel
-from models.product import Product
+from os import environ
 
 class DBStorage:
-    """Interacts with MongoDB Database"""
+    """Interacts with MongoDB Database
+    Adopting Singleton connection to prevent multiple connections
+    """
 
-    def __init__(self):
-        """Creating the db connecion"""
-        mongoengine.register_connection(alias='core', name='farmer_market')
 
+    _instance = None
+    db_name = environ.get('DB_NAME', 'farmer_market')
+    host = environ.get('HOST', '127.0.0.1')
+    port = int(environ.get('PORT', '27017'))
+    meta = {'db_alias': 'core'}
+
+    def __new__(cls):
+        if not cls._instance:
+            cls._instance = super(DB_Store, cls).__new__(cls)
+            cls._instance.connect()
+        return cls._instance
+    
+    @classmethod
+    def connect(cls):
+        mongoengine.register_connection(alias='core', name=cls.db_name, host=cls.host, port=cls.port)
+        mongoengine.connect(cls.db_name, alias='core')
+
+    @classmethod
+    def close_connection(cls):
+        mongoengine.disconnect(cls.db_name)
+
+
+engine = DB_Store()
